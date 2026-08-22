f46418db249b858eada51cb8e4a083523473c58a
pias.roy <pias.kuet@gmail.com>
2026-08-23 00:08:40 +0600

    DbSeeder implemented, Auth pipeline added

---
+++ CourseManagement.API/DbSeeder.cs
@@ -0,0 +1,82 @@
+﻿using CourseManagement.Business.Constants;
+using CourseManagement.Business.DTOs.UserDTOs;
+using CourseManagement.Business.Services.Interfaces;
+using CourseManagement.Domain.Entities;
+using CourseManagement.Infrastructure.ApplicationData;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseManagement.API;
+
+public class DbSeeder
+{
+    private readonly IAuthService authService;
+    private readonly ApplicationDbContext dbContext;
+
+    public DbSeeder(
+        IAuthService authService,
+        ApplicationDbContext dbContext)
+    {
+        this.authService = authService;
+        this.dbContext = dbContext;
+    }
+
+    public async Task DbSeed()
+    {
+        await this.dbContext.Database.MigrateAsync();
+        await this.SeedRoles();
+        await this.SeedAdminUsers();
+    }
+
+    private async Task SeedRoles()
+    {
+        if (await this.dbContext.UserRoles.AnyAsync())
+        {
+            return;
+        }
+
+        var roles = new List<UserRole>
+        {
+            new()
+            {
+                RoleId = Guid.NewGuid(),
+                RoleName = UserRoles.Admin
+            },
+            new()
+            {
+                RoleId = Guid.NewGuid(),
+                RoleName = UserRoles.Staff
+            },
+            new()
+            {
+                RoleId = Guid.NewGuid(),
+                RoleName = UserRoles.Instructor
+            },
+            new()
+            {
+                RoleId = Guid.NewGuid(),
+                RoleName = UserRoles.Student
+            }
+        };
+
+        await this.dbContext.UserRoles.AddRangeAsync(roles);
+        await this.dbContext.SaveChangesAsync();
+    }
+
+    private async Task SeedAdminUsers()
+    {
+        if (await this.dbContext.Users.AnyAsync())
+        {
+            return;
+        }
+
+        var createUser = new CreateUserRequest
+        {
+            FirstName = "Pias",
+            LastName = "Roy",
+            EmailAddress = "pias.roy@admin.com",
+            Password = "password12345678"
+        };
+
+        await this.authService.CreateUserAsync(createUser, [UserRoles.Admin]);
+    }
+}
--- CourseManagement.API/Program.cs
+++ CourseManagement.API/Program.cs
@@ -1,8 +1,13 @@
-using Course_Management_Service.Extensions;
+using CourseManagement.API;
+using CourseManagement.API.Extensions;
+using CourseManagement.Business.Factories;
+using CourseManagement.Business.Options;
 using CourseManagement.Infrastructure.ApplicationData;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using Serilog;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +18,23 @@ builder.Services.AddOpenApi();
 builder.Services.AddSerilogLogging(configuration);
 builder.Services.AddEFServices(configuration);
 builder.Services.AddControllers();
+builder.Services.AddCustomServices();
+
+builder.Services.Configure<AuthOptions>(configuration.GetSection(nameof(AuthOptions)));
+
+var authSection = configuration.GetSection(nameof(AuthOptions));
+var authOptions = authSection.Get<AuthOptions>()
+            ?? throw new InvalidOperationException("JWT configuration section is missing or invalid.");
+
+var secretKey = Encoding.UTF8.GetBytes(authOptions.Secret);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = TokenValidationParametersFactory.Create(authOptions);
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -25,11 +47,15 @@ if (app.Environment.IsDevelopment())
 
 await using (var scope = app.Services.CreateAsyncScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    await db.Database.MigrateAsync();
+    var dbSeeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
+    await dbSeeder.DbSeed();
 }
 
 app.UseSerilogRequestLogging();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
