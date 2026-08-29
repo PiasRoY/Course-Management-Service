397416755b85fc36ecb781b27729827dc7f25aa9
pias.roy <pias.kuet@gmail.com>
2026-08-29 12:54:02 +0600

    nullable roles

--- CourseManagement.Domain/Common/ICurrentUserContext.cs
+++ CourseManagement.Domain/Common/ICurrentUserContext.cs
@@ -6,6 +6,6 @@ public interface ICurrentUserContext
 {
     public string? UserId { get; }
     public string? Email { get; }
-    public IEnumerable<UserRoles> Roles { get; }
+    public IEnumerable<UserRoles>? Roles { get; }
     void SetUserContext(string? userId, string? email, IEnumerable<UserRoles>? roles);
 }
--- CourseManagement.Infrastructure/ApplicationData/CurrentUserContext.cs
+++ CourseManagement.Infrastructure/ApplicationData/CurrentUserContext.cs
@@ -7,12 +7,12 @@ public class CurrentUserContext : ICurrentUserContext
 {
     public string? UserId { get; private set; }
     public string? Email { get; private set; }
-    public IEnumerable<UserRoles> Roles { get; private set; } = [];
+    public IEnumerable<UserRoles>? Roles { get; private set; }
 
     public void SetUserContext(string? userId, string? email, IEnumerable<UserRoles>? roles)
     {
         UserId = userId;
         Email = email;
-        Roles = roles ?? [];
+        Roles = roles;
     }
 }
